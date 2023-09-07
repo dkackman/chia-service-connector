df6d292b5df70f425beb052c141e9ef6d7a7e398
Don Kackman <dkackman@gmail.com>
2023-09-07 09:25:04 -0500

    initial commit

---
+++ src/lib/index.js
@@ -0,0 +1,33 @@
+const os = require("os");
+const path = require("path");
+const ChiaConnection = require("./chia_connection");
+const {
+  ServiceNames,
+  DefaultServicePorts,
+  createConnection,
+} = require("./connection_factory");
+
+let chiaRoot = null;
+
+const getChiaRoot = () => {
+  if (chiaRoot) {
+    return chiaRoot;
+  }
+
+  if (process.env.CHIA_ROOT) {
+    chiaRoot = path.resolve(process.env.CHIA_ROOT);
+  } else {
+    const homeDir = os.homedir();
+    chiaRoot = path.resolve(`${homeDir}/.chia/mainnet`);
+  }
+
+  return chiaRoot;
+};
+
+module.exports = {
+  getChiaRoot,
+  createConnection,
+  ChiaConnection,
+  ServiceNames,
+  DefaultServicePorts,
+};
