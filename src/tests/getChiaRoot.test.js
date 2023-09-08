084a67a3ec6aa0da62ba83ceed02d2c036b9d189
Don Kackman <dkackman@gmail.com>
2023-09-07 22:50:08 -0500

    commenting

--- src/tests/getChiaRoot.test.js
+++ src/tests/getChiaRoot.test.js
@@ -8,18 +8,30 @@ const expect = chai.expect;
 describe("chia-service-connector", () => {
   describe("getChiaRoot", () => {
     it("should return default path when CHIA_ROOT not set", () => {
+      // arrange
       __resetChiaRoot();
       delete process.env.CHIA_ROOT;
       const expected = path.resolve(`${os.homedir()}/.chia/mainnet`);
+
+      // act
       const chiaRoot = getChiaRoot();
+
+      // assert
       expect(chiaRoot).to.equal(expected);
     });
     it("should return CHIA_ROOT when set", () => {
+      // arrange
       __resetChiaRoot();
       process.env.CHIA_ROOT = "CHIA_ROOT__TEST";
       const expected = path.resolve(process.env.CHIA_ROOT);
+
+      // act
       const chiaRoot = getChiaRoot();
+
+      // assert
       expect(chiaRoot).to.equal(expected);
+
+      // cleanup
       delete process.env.CHIA_ROOT;
     });
   });
